335b558818cf23dddbe875c8f5017a48e4650c22
Arie Twigt <ariekanarie@twigtd4.nl>
2022-09-22 10:54:17 +0200

    added 3c

--- 3c_assignment.py
+++ 3c_assignment.py
@@ -6,3 +6,7 @@ files_folders = os.listdir()
 files_folders_str =  " - 📄" + "\n - 📄 ".join(files_folders)
 
 print(files_folders_str)
+
+# if you would like to check if a an element in the list is a file or folder
+# - os.path.isfile("my_script.py")
+# - os.path.isdir("my_folder")
